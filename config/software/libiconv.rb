a51996589931ef6c674902a8897f029d90a9161b
Carl Perry <caperry@edolnx.net>
2016-12-30 21:36:30 -0600

    Replace md5 checksum with sha256 checksum

--- config/software/libiconv.rb
+++ config/software/libiconv.rb
@@ -24,7 +24,7 @@ license_file "COPYING.LIB"
 dependency "config_guess"
 
 source url: "https://ftp.gnu.org/pub/gnu/libiconv/libiconv-#{version}.tar.gz",
-       md5: "e34509b1623cec449dfeb73d7ce9c6c6"
+       sha256: "72b24ded17d687193c3366d0ebe7cde1e6b18f0df8c55438ac95be39e8a30613"
 
 relative_path "libiconv-#{version}"
 
